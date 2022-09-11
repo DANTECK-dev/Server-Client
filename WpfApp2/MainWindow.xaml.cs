75d572282e02407b78b6e69c4a4e18ee906d2839
Александр Немеров <nemerov_a@mail.ru>
2022-09-11 14:58:06 +0800

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Server server;
+        public MainWindow()
+        {
+            InitializeComponent();
+            server = new Server();
+            server.Visibility = Visibility.Visible;
+        }
+
+        private void Sender_Click(object sender, RoutedEventArgs e)
+        {
+            server.Read();
+            OutputText.Content = server.InputText.Content.ToString();
+            OutputText.Content += "\nDone";
+        }
+    }
+}
---
+++ WpfApp2/Server.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+using System.IO;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Server.xaml
+    /// </summary>
+    public partial class Server : Window
+    {
+        static Semaphore sem;
+        Thread myThread;
+        FileStream? fstream = null;
+        string path = "C:\\lbr\\text.txt";
+        //int count = 2;
+        public Server()
+        {
+            InitializeComponent();
+            sem = new Semaphore(1, 2);
+            myThread = new Thread(Read);
+            myThread.Name = "поток 1";
+            myThread.Start();
+            InputText.Content = "";
+
+        }
+
+        public void Read()
+        {
+            sem.WaitOne();
+            
+
+            try
+            {
+                fstream = new FileStream(path, FileMode.Open);
+                //string[] readText = File.ReadAllLines(path);
+                byte[] buffer = new byte[fstream.Length];
+                fstream.Read(buffer, 0, buffer.Length);
+                string readText = Encoding.Default.GetString(buffer);
+                char[] arr = readText.ToCharArray();
+                InputText.Content = "";
+                int counter = 0;
+                for (int i = 0; i < readText.Length; i++)
+                {
+                    if (char.IsLetter(arr[i]))
+                    {
+                        InputText.Content += arr[i].ToString();
+                        counter++;
+                    }
+                    if (counter == 100)
+                    {
+                        InputText.Content += "\n";
+                        counter = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fstream?.Close();
+            }
+            sem.Release();
+        }
+    }
+}
